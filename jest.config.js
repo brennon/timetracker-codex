fcb1cbc4f9fbf16498745cf9e8a4ed38efbb44c8
Brennon Bortz <brennon@brennonbortz.com>
2025-05-19 19:29:09 -0700

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^uuid$': '<rootDir>/node_modules/uuid/dist/index.js',
+  },
+  setupFiles: ['./jest.setup.ts'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
 };
